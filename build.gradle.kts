0d7494de5fa493dcbcb2ff971eee92bcf216db6c
Helmy <mohamed0132525715@gmail.com>
2021-11-28 18:03:57 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.6.0"
+}
+
+group = "com.example"
+version = "0.0.1"
+application {
+    mainClass.set("com.example.ApplicationKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-gson:$ktor_version")
+    implementation("io.ktor:ktor-auth:$ktor_version")
+    implementation("io.ktor:ktor-auth-jwt:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    implementation("com.microsoft.sqlserver:mssql-jdbc:6.4.0.jre7")
+    implementation("org.jetbrains.exposed:exposed-core:0.36.1")
+    implementation("org.jetbrains.exposed:exposed-dao:0.36.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.36.1")
+
+    implementation("com.google.code.gson:gson:2.8.9")
+}
---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,17 @@
+package com.example
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        routing {
+            get("/") {
+                call.respondText("Hello World!")
+            }
+        }
+    }.start(wait = true)
+}
